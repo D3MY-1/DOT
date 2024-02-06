11cb77a44c1710460a8b0f652626001f644f71d4
Артур Малиновски <arturmalinnka@gmail.com>
2024-02-06 21:49:10 +0200

    Implemented Filters.

---
+++ ViewModels/FilterItems.cs
@@ -0,0 +1,90 @@
+﻿using DOT.Models;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace DOT.ViewModels
+{
+    public class FilterItem : ReactiveObject
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => this.RaiseAndSetIfChanged(ref _name, value);
+        }
+
+        private readonly SecondViewModel _secondViewModel;
+        
+        public FilterItem(string nme,SecondViewModel svm)
+        {
+            _name = nme;
+            _secondViewModel = svm;
+            this.WhenAnyValue(x => x.IsChecked)
+                .Subscribe(StartSearch!);
+            
+        }
+
+        private void StartSearch(bool b)
+        {
+           _secondViewModel.PerformSearch();
+        }
+
+        private bool _isChecked;
+
+        public bool IsChecked
+        {
+            get => _isChecked;
+            set => this.RaiseAndSetIfChanged(ref _isChecked, value);
+        }
+    }
+
+    public class FilterUpperItem : ViewModelBase
+    {
+        private string _name;
+
+        private ObservableCollection<FilterItem> _values;
+
+        public string Name
+        {
+            get => _name;
+            set => this.RaiseAndSetIfChanged(ref _name, value);
+        }
+
+        public ObservableCollection<FilterItem> Values
+        {
+            get => _values;
+            set => this.RaiseAndSetIfChanged(ref _values, value);
+        }
+
+
+        public List<string> GetToggled()
+        {
+            var ret = new List<string>();
+            foreach (var i in _values)
+            {
+                if(i.IsChecked)
+                {
+                    ret.Add(i.Name);
+                }
+            }
+            return ret;
+        }
+
+        public FilterUpperItem(string Filter, List<FilterItem> values)
+        {
+            _name = Filter;
+
+            _values = new ObservableCollection<FilterItem>(values);
+
+            
+        }
+    }
+}
--- ViewModels/SecondViewModel.cs
+++ ViewModels/SecondViewModel.cs
@@ -9,6 +9,10 @@ using System.Reactive;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Dialogs.Internal;
+using System.Runtime.InteropServices;
+using DynamicData;
 
 namespace DOT.ViewModels
 {
@@ -16,9 +20,12 @@ namespace DOT.ViewModels
     {
         private ObservableCollection<ItemViewModel> _buttons;
 
+
         private string _searchText;
 
         private List<ItemViewModel> _items;
+
+        private ObservableCollection<FilterUpperItem> _filterItems;
 
         public ReactiveCommand<Unit, Unit> Command { get; }
 
@@ -34,40 +41,131 @@ namespace DOT.ViewModels
             set => this.RaiseAndSetIfChanged(ref _buttons, value);
         }
 
+        public ObservableCollection<FilterUpperItem> FilterItems
+        {
+            get => _filterItems;
+            set => this.RaiseAndSetIfChanged(ref _filterItems, value);
+        }
+
         
 
         private DatabaseLoader loader;
+
+        
 
         public SecondViewModel(MainViewModel mvm,Models.Type type)
         {
             _ = Logger.Instance.Log($"Initialized new SecondViewModel with this type : {type.Name}");
             var v = type.Items;
 
+
+            //var filterValue = new Dictionary<string, HashSet<string>>();
+             var filterValue = new List<HashSet<string>>();
+
+            foreach(var filter in type.Filters)
+            {
+                filterValue.Add(new HashSet<string>());   //This needs to be remade
+            }
+
+            
+
             _items = new List<ItemViewModel>();
             foreach (var a in v)
             {
                 _items.Add(new ItemViewModel(a,mvm));
+                for (var i = 0;i < type.Filters.Count;i++) 
+                {
+                    filterValue[i].Add(a.FilterValues[i]); // Too complicated
+                }
             }
+
+            _filterItems = new ObservableCollection<FilterUpperItem>();
+
+            for (var i = 0;i < filterValue.Count;i++)
+            {
+                var items = new List<FilterItem>();
+                foreach (var a in filterValue[i])
+                    items.Add(new FilterItem(a,this));
+                _filterItems.Add(new FilterUpperItem(type.Filters[i], items));
+            }
+
+
+            
+            
+            
+            
             Buttons = new ObservableCollection<ItemViewModel>(_items);
 
             this.WhenAnyValue(x => x.SearchText)
                 .ObserveOn(RxApp.MainThreadScheduler)
-                .Subscribe(PerformSearch!);
+                .Subscribe(StartSearch!);
             Command = ReactiveCommand.Create(() => mvm.ChangeViewModel(MainViewModel.ViewModelEnum.First, null));
         }
 
-        public void PerformSearch(string s) // Need more advanced algorithm.
+        private void StartSearch(string s)
         {
-            if (s == null) { return; }
+            PerformSearch();
+        }
+
+        public void PerformSearch() // Need more advanced algorithm.
+        {
+            if(Buttons == null)
+                return;
+            var s = SearchText;
+
             Buttons.Clear();
+
+            bool performFilterSearch = false;
+            foreach (var item in _filterItems) 
+            {
+                if(item.GetToggled().Count > 0) {  performFilterSearch = true; break; }
+            }
+
+            if (!performFilterSearch && s == null)
+            {
+                Buttons.Add(_items); 
+                return;
+            }
+            
+
+            
+
             foreach (var i in _items)
             {
-                if (i.Name.ToUpper().Contains(s.ToUpper()))
+                if(performFilterSearch)
                 {
-                    Buttons.Add(i);
+                    var values = i.GetFilterValues();
+                    bool isValid = true;
+                    for (var j = 0; j < values.Count; j++)
+                    {
+                        var v = FilterItems[j].GetToggled();
+                        if ((v.Count == 0) || (v.Count > 0 && v.Contains(values[j])))
+                            continue;
+                        
+                        isValid = false;
+                        break;
+                    }
+                    if (isValid)
+                        IfSimilarADD(i, s);
                 }
+                else
+                    IfSimilarADD(i, s);
             }
 
         }
+
+
+        private void IfSimilarADD(ItemViewModel item, string s)
+        {
+            if (s == null)
+            {
+                Buttons.Add(item);
+                return;
+            }
+            
+            if (item.Name.ToUpper().Contains(s.ToUpper()))
+                Buttons.Add(item);
+        }
     }
+
 }
